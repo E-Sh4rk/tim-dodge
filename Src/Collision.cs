41633713c059082c12e655c1e54b55d1da0c6e45
Mickaël LAURENT <esh4rk@gmail.com>
2017-10-09 15:13:52 +0200

    Added Map class + collision updates

--- Src/Collision.cs
+++ Src/Collision.cs
@@ -4,9 +4,17 @@ namespace tim_dodge
 {
 	public static class Collision
 	{
+		/**
+		 * If the intersection is not null, return a unit vector in the direction that the collision force would have.
+		 **/
 		public static Vector2? rect_collision(Rectangle r1, Rectangle r2)
 		{
-			// TODO
+			if (r1.Intersects(r2))
+			{
+				Vector2 dir = r2.Center.ToVector2() - r1.Center.ToVector2();
+				dir.Normalize();
+				return dir;
+			}
 			return null;
 		}
 		public static bool sprite_collision(Sprite s1, Sprite s2)
@@ -16,7 +24,11 @@ namespace tim_dodge
 		}
 		public static Vector2? optimized_sprite_collision(Sprite s1, Sprite s2)
 		{
-			// TODO
+			Vector2? rect_col = rect_collision(s1.RectOfSprite(), s2.RectOfSprite());
+			if (rect_col == null)
+				return null;
+			if (sprite_collision(s1, s2))
+				return rect_col;
 			return null;
 		}
 	}
---
+++ Src/Map.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace tim_dodge
+{
+	public class Map
+	{
+		public Rectangle[] roofs
+		{
+			get;
+			protected set;
+		}
+		public Rectangle[] grounds
+		{
+			get;
+			protected set;
+		}
+		public Rectangle[] leftWalls
+		{
+			get;
+			protected set;
+		}
+		public Rectangle[] rightWalls
+		{
+			get;
+			protected set;
+		}
+
+		public Map()
+		{
+			// TODO: Different maps system
+			roofs = new Rectangle[] { };
+			leftWalls = new Rectangle[] { };
+			rightWalls = new Rectangle[] { };
+			grounds = new Rectangle[] { new Rectangle(0, 538, TimGame.WINDOW_WIDTH, 100) };
+		}
+	}
+}
