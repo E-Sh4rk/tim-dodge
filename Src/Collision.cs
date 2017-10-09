3259ce0c0c20816bb09c88e6e9ed5c23ddf24f2f
Mickaël LAURENT <esh4rk@gmail.com>
2017-10-09 12:59:47 +0200

    base structure for physics

--- Src/Collision.cs
+++ Src/Collision.cs
@@ -4,5 +4,20 @@ namespace tim_dodge
 {
 	public static class Collision
 	{
+		public static Vector2? rect_collision(Rectangle r1, Rectangle r2)
+		{
+			// TODO
+			return null;
+		}
+		public static bool sprite_collision(Sprite s1, Sprite s2)
+		{
+			// TODO
+			return false;
+		}
+		public static Vector2? optimized_sprite_collision(Sprite s1, Sprite s2)
+		{
+			// TODO
+			return null;
+		}
 	}
 }
---
+++ Src/PhysicalObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace tim_dodge
+{
+	/**
+	 * An object that is subject to collisions and gravity.
+	**/
+	public class PhysicalObject : GameObject
+	{
+		public PhysicalObject()
+		{
+		}
+
+		// Position is part of GameObject
+		protected Vector2 Velocity;
+		protected Vector2 Acceleration;
+		protected Vector2 Friction;
+		protected List<Vector2> forces;
+
+		protected List<int> reciprocal_forces; // To exclude for collisions because already computed
+
+		public int mass
+		{
+			get;
+			protected set;
+		}
+		public Sprite sprite
+		{
+			get;
+			protected set;
+		}
+
+		public void ApplyNewForce(Vector2 force, int reciprocal_id = -1)
+		{
+			// TODO
+		}
+
+		public void UpdatePosition()
+		{
+			// Compute collisions with other physical objects and, depending on the relative direction of the center of the sprite,
+			// compute what resulting force in this direction need to be applied (depending on the difference of velocity in this direction and
+			// the min mass of the two objects).
+			// Add this resulting force to the both objects involved.
+			// TODO
+
+			// Compute new acceleration, velocity and position by taking into account all the forces
+			// TODO
+
+			// Compute collisions with the map (which has infinite mass), and apply the new resulting forces
+			// TODO
+		}
+	}
+}
