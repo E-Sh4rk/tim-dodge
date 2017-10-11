88238fa7a509bc219b044c42b37caf61f01315f7
AlexisBaudin <alexis.baudin@live.fr>
2017-10-11 17:48:35 +0200

    Avec la classe Enemies

---
+++ Src/Enemies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+using Microsoft.Xna.Framework;
+
+namespace tim_dodge
+{
+	public class Enemies
+	{
+		private Timer timer;
+		private Random random;
+		public List<Enemy> ListEnemies
+		{
+			get;
+		}
+
+		public Enemies()
+		{
+			random = new Random();
+			timer = new Timer();
+			timer.Start();
+			timer.Interval = 1000;
+			timer.Elapsed += GenerateEnemy;
+			ListEnemies = new List<Enemy>();
+		}
+
+		private void GenerateEnemy(Object source, System.Timers.ElapsedEventArgs e)
+		{
+			int X = random.Next(0, TimGame.WINDOW_WIDTH);
+			Enemy enemy = new Enemy(TimGame.bombPicture, null, new Vector2(X, 0));
+			ListEnemies.Add(enemy);
+		}
+	}
+}
