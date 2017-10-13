5f68d62d0fb20a1a31a4f276d26fbd974a828c07
Mickaël LAURENT <esh4rk@gmail.com>
2017-10-13 15:06:28 +0200

    bombs aim player

--- Src/Enemies.cs
+++ Src/Enemies.cs
@@ -15,17 +15,19 @@ namespace tim_dodge
 		}
 		private Texture texture = null;
 		private Map map = null;
+		private Player player = null;
 
-		public Enemies(Texture texture, Map map)
+		public Enemies(Texture texture, Map map, Player player)
 		{
 			time = 0;
 			this.texture = texture;
 			this.map = map;
+			this.player = player;
 			random = new Random();
 			ListEnemies = new List<Enemy>();
 		}
 
-		const int interval = 1;
+		const float interval = 0.25f;
 
 		private float time;
 		public void UpdateEnemies(GameTime gt)
@@ -34,8 +36,11 @@ namespace tim_dodge
 
 			while (time > interval)
 			{
-				int X = random.Next(0, TimGame.WINDOW_WIDTH);
+				int X = random.Next(0, TimGame.WINDOW_WIDTH-texture.Image.Width);
 				Enemy enemy = new Enemy(texture, null, new Vector2(X, -30));
+				Rectangle r1 = new Rectangle(enemy.Position.ToPoint(), enemy.Size);
+				Rectangle r2 = new Rectangle(player.Position.ToPoint(), player.Size);
+				enemy.ApplyNewImpulsion(new Vector2(Collision.direction_between(r1, r2, false).X * 0.04f, 0));
 				ListEnemies.Add(enemy);
 				time -= interval;
 			}
