d4e9b0f94c07dc96475d7de6e275c6279626aaa8
AlexisBaudin <alexis.baudin@live.fr>
2017-10-13 16:21:35 +0200

    Classe Stat + début life

--- Src/GameInstance.cs
+++ Src/GameInstance.cs
@@ -12,31 +12,37 @@ namespace tim_dodge
 	{
 		public Player player;
 		private Enemies enemies;
-		public Score score;
-		private SpriteFont fontScore;
 		public Map map { get; }
 
+		public Stat score;
+		private Vector2 PositionScore;
+
+		public Stat life;
+		private Vector2 PositionLife;
+		private int InitialLife;
+
+		private SpriteFont fontDisplay;
 		private ContentManager Content;
 
 		public GameInstance(ContentManager Content)
 		{
 			map = new Map();
 			this.Content = Content;
-
-			LoadContent();
-		}
-
-		private void LoadContent()
-		{
 			player = new Player(new Texture(Content.Load<Texture2D>("character/Tim")), new Sprite("Content.character.TimXml.xml"),
-					map, new Vector2(500, 250));
-
-			fontScore = Content.Load<SpriteFont>("SpriteFonts/Score");
-			score = new Score(fontScore);
-
+		map, new Vector2(500, 250));
 			enemies = new Enemies(new Texture(Content.Load<Texture2D>("objects/bomb")), this);
+			fontDisplay = Content.Load<SpriteFont>("SpriteFonts/Score");
 
+			InitialLife = 100;
+
+			PositionScore = new Vector2(30, 20);
+			PositionLife = new Vector2(30, 20 + fontDisplay.MeasureString("S").Y);
+
+			score = new Stat(fontDisplay, "Score : ", PositionScore, Color.Black, 0);
+			life = new Stat(fontDisplay, "Life : ", PositionLife, Color.Red, InitialLife);
 		}
+
+
 
 		public void Update(GameTime gameTime)
 		{
@@ -64,6 +70,7 @@ namespace tim_dodge
 			foreach (Enemy en in enemies.ListEnemies)
 				en.Draw(spriteBatch);
 			score.Draw(spriteBatch);
+			life.Draw(spriteBatch);
 		}
 	}
 }
