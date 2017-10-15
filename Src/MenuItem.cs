34b90542fdd3c8e7dbcf48bc0011aa6563149805
AlexisBaudin <alexis.baudin@live.fr>
2017-10-15 20:44:01 +0200

    The class MenuItem that I forgot

---
+++ Src/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace tim_dodge
+{
+	public class MenuItem : Item
+	{
+		public delegate void ItemSelected();
+		public event ItemSelected LaunchSelection;
+
+		public MenuItem(String Text, SpriteFont fontItem, Color Color) : base(Text, fontItem, Color)
+		{
+		}
+
+		public void LaunchSelecion()
+		{
+			LaunchSelection();
+		}
+
+
+	}
+}
