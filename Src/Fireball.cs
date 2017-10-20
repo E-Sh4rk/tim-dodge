edc374b1ec30394ddba87453328f712b9d2b7815
Mickaël LAURENT <esh4rk@gmail.com>
2017-10-20 14:27:46 +0200

    fireball disparition improved

--- Src/Fireball.cs
+++ Src/Fireball.cs
@@ -11,10 +11,40 @@ namespace tim_dodge
 			Mass = 5;
 		}
 
+		protected void autoDestruct(GameTime gameTime)
+		{
+			if (Ghost)
+			{
+				if (Sprite.NowFrame() >= Sprite.NumberOfFrames() - 1)
+					Dead = true;
+			}
+		}
+
+		protected bool damaged = false;
 		protected override void ApplyCollision(Vector2 imp, int id, GameTime gt)
 		{
 			base.ApplyCollision(imp, id, gt);
-			Dead = true;
+			// Destroy the fireball if collision with something else
+			damaged = true;
+		}
+
+		public override void UpdatePosition(List<PhysicalObject> objects, Map map, GameTime gameTime)
+		{
+			if (damaged)
+				destructionMode(gameTime);
+			base.UpdatePosition(objects, map, gameTime);
+			autoDestruct(gameTime);
+		}
+
+		public override void destructionMode(GameTime gt)
+		{
+			if (!Ghost)
+			{
+				GameManager.sounds.playSound(Sound.SoundName.explosion);
+				ChangeSpriteState(3);
+				Ghost = true;
+				Velocity = new Vector2(0, 0);
+			}
 		}
 	}
 }
